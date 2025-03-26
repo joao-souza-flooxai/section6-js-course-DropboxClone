1fac0d1d12ca0ac2512ae9da5a78ae3c88ff7d40
joao-souza-flooxai <joao.souza.flooxai@gmail.com>
2025-03-26 13:32:50 -0300

    chore: Added formidable dependency, to work with form data. feat: Implemented the Upload bar and time left to upload an archive as well as its route('post' /upload) which goes to a directory called /upload, using formidable, for now.

--- curso-javascript-projeto-dropbox-clone/app/public/src/controller/DropBoxController.js
+++ curso-javascript-projeto-dropbox-clone/app/public/src/controller/DropBoxController.js
@@ -5,6 +5,10 @@ class DropBoxController{
       this.btnSendFileEl = document.querySelector('#btn-send-file');
       this.inputFilesEl = document.querySelector('#files');
       this.snackModalEl = document.querySelector('#react-snackbar-root');
+      this.progressBarEl = this.snackModalEl.querySelector(".mc-progress-bar-fg");
+      this.namefileEl = this.snackModalEl.querySelector(".filename");
+      this.timeleftEl = this.snackModalEl.querySelector(".timeleft");
+
       this.initEvents();  
     }
 
@@ -13,11 +17,20 @@ class DropBoxController{
             this.inputFilesEl.click();
         });
         this.inputFilesEl.addEventListener('change', event =>{
-            console.log(event.target.files);
             this.uploadTask(event.target.files);
-            this.snackModalEl.style.setProperty('display', 'block', 'important');
+            this.showUploadBar();
+            this.inputFilesEl.value = '';
         });
     }
+
+    showUploadBar(show = true){
+        if(show)
+            this.snackModalEl.style.setProperty('display', 'block', 'important');
+        else
+            this.snackModalEl.style.setProperty('display', 'none');
+
+    }
+
 
     uploadTask(files){
 
@@ -30,6 +43,8 @@ class DropBoxController{
                 ajax.open('POST', '/upload');
 
                 ajax.onload = event =>{
+
+                    this.showUploadBar(false);
                     try{
                         resolve(JSON.parse(ajax.responseText));
                     }catch(e){
@@ -37,12 +52,20 @@ class DropBoxController{
                     }
                 };
                 ajax.onerror =  event =>{
-                    reject(event);    
+                    this.showUploadBar(false);  
+                    reject(event); 
+                };
+
+                //Capturando o progresso do Upload para atulizar a barra de upload(react-snack)
+                ajax.upload.onprogress = event =>{
+                    this.uploadProgress(event, file);
+                    console.log(event);
                 };
 
                 let formData = new FormData();
                 formData.append('input-file', file);
 
+                this.startUploadTime = Date.now();
                 ajax.send(formData);
 
             }));
@@ -51,4 +74,42 @@ class DropBoxController{
         return Promise.all(promises);
     }
 
+    uploadProgress(event, file ){
+
+        ///Quantos de bits foram enviados a té o momento
+        let loaded = event.loaded;
+        //Quantiade total que bits que precisam ser enviados
+        let total = event.total;
+        //Fazendo a porcentagem disso
+        let porcent = parseInt((loaded / total) * 100);
+        //Capturando o momento do upload e diminuindo com o momento que o processo começou
+        let timeSpent = Date.now() - this.startUploadTime;
+        //Regra de três para calcular quanto tempo irá faltar(timeLeft) com base no tempo decorrido entre os uploads(timeSpent)
+        let timeLeft = ((100 - porcent) * timeSpent) / porcent;
+        //Colocando a porcentagem no elemento correto na view no hmtl.
+        this.progressBarEl.style.width = `${porcent}%`;
+        //Colocando o nome do arquivo no elemento correto na view no hmtl.
+        this.namefileEl.innerHTML = file.name;
+        //Colocando o tempo de upload elemento correto na view no hmtl.
+        this.timeleftEl.innerHTML =  this.formatTimeLeftUpload(timeLeft) ;      
+    }
+
+    //Formatando o tempo com base no tempo decorrido.
+    formatTimeLeftUpload(duration){
+      let seconds = parseInt((duration/1000) % 60);
+      let minutes = parseInt((duration/(1000 * 60)) % 60); 
+      let hours = parseInt((duration/(1000 * 60 * 60)) % 24); 
+      let timeLeftFormated ;
+
+      if(hours > 0)
+        return `${hours} horas, ${minutes} minutos e ${seconds} segundos`;
+      if(minutes>0)
+        return `${minutes} minutos e ${seconds} segundos`;
+      if(seconds > 0)
+        return `${seconds} segundos`;
+
+      return 0;
+    }
+
 }
+
--- curso-javascript-projeto-dropbox-clone/app/routes/index.js
+++ curso-javascript-projeto-dropbox-clone/app/routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var formidable = require('formidable');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/upload', (req,res)=>{
+  
+  //Aqui é definido o diretorio que o upload será recebido.
+  let form = new formidable.IncomingForm({
+    uploadDir: './upload',
+    keepExtensions: true
+  });
+
+  /*
+    Aqui, o que é recebido é interpretado(parse)  e é feito a separação de campos e files. 
+    Nesse caso, só é nos interessa files.
+  */
+  form.parse(req, (err, fields, files)=>{
+    res.json({files});
+  });
+
+
 });
 
 module.exports = router;
