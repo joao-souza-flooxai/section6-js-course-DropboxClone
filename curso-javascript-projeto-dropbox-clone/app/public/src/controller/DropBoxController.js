f2dc8c29a1a57c55b1df977a5516d82e3c858fc3
joao-souza-flooxai <joao.souza.flooxai@gmail.com>
2025-03-27 02:33:01 -0300

    feat: Added the creation of Folders and the navgation and logic between them.

--- curso-javascript-projeto-dropbox-clone/app/public/src/controller/DropBoxController.js
+++ curso-javascript-projeto-dropbox-clone/app/public/src/controller/DropBoxController.js
@@ -6,6 +6,7 @@ class DropBoxController{
       this.onselectionchange = new Event('selectionchange');
       this.currentFolder = ['root'];
 
+      this.naveEl = document.querySelector('#browse-location');
       this.btnSendFileEl = document.querySelector('#btn-send-file');
       this.inputFilesEl = document.querySelector('#files');
       this.snackModalEl = document.querySelector('#react-snackbar-root');
@@ -20,7 +21,7 @@ class DropBoxController{
       //Metodos de início
       this.connectFirebase();
       this.initEvents();  
-      this.readFiles();
+      this.openFolder();
     }
 
     connectFirebase(){
@@ -38,8 +39,11 @@ class DropBoxController{
     }
 
 
-    getFirebaseRef() {
-        return firebase.database().ref("files");
+    getFirebaseRef(path) {
+      
+      if(!path) path = this.currentFolder.join('/');
+      
+      return firebase.database().ref(path);
     }
 
    
@@ -82,8 +86,23 @@ class DropBoxController{
       this.btnRename.style.setProperty('display', 'block', 'important');
     }
 
+    createFolder(name, path){
+      this.getFirebaseRef().push().set({
+        name,
+        type:'folder',
+        path
+      });
+    }
+
     initEvents(){
       
+      this.btnNewFolder.addEventListener("click", (e) => {
+        let name = prompt('Nome da nova pasta:');
+        if(name){
+          this.createFolder(name, this.currentFolder.join('/'));
+        }
+      });
+
       this.btnDelete.addEventListener("click", (e) => {
         if(confirm("Deseja realmente excluir?")){
           this.removeTask()
@@ -445,7 +464,73 @@ class DropBoxController{
         }
       }
 
+      openFolder(){
+
+        if(this.lastFolder) this.getFirebaseRef(this.lastFolder)
+        .off('value');
+        this.renderNav();
+        this.readFiles();
+      
+      }
+
+      renderNav(){
+        let nav = document.createElement('nav');
+        let path = [];
+        for(let i = 0; i< this.currentFolder.length; i++){
+
+          let folderName = this.currentFolder[i];
+          let span = document.createElement('span');
+
+          path.push(folderName);
+
+          if((i + 1) === this.currentFolder.length){
+
+            span.innerHTML = folderName;
+
+          }else{
+
+            span.className = 'breadcrumb-segment__wrapper'
+            span.innerHTML = `
+              <span class="ue-effect-container uee-BreadCrumbSegment-link-0">
+                <a href="#" data-path="${path.join('/')}" class="breadcrumb-segment">${folderName}</a>
+              </span>
+              <svg width="24" height="24" viewBox="0 0 24 24" class="mc-icon-template-stateless" style="top: 4px; position: relative;">
+                <title>arrow-right</title>
+                <path d="M10.414 7.05l4.95 4.95-4.95 4.95L9 15.534 12.536 12 9 8.464z" fill="#637282" fill-rule="evenodd"></path>
+              </svg>
+            `;
+
+          }
+          nav.appendChild(span);          
+        }
+        this.naveEl.innerHTML = nav.innerHTML;
+
+        this.naveEl.querySelectorAll('a').forEach(a=>{
+          a.addEventListener('click', e=>{
+            e.preventDefault();
+            this.currentFolder = a.dataset.path.split('/');
+            this.openFolder();
+          });
+        });
+      }
+
       initEventsLi(li) {
+
+        li.addEventListener('dblclick', e => {
+          
+          let file = JSON.parse(li.dataset.file);
+
+          switch(file.type){
+            case 'folder':
+              this.currentFolder.push(file.name);
+              this.openFolder();
+            break;
+
+            default:
+          }
+
+        });
+
         li.addEventListener('click', e => {
      
           //Se o shift estiver precionado, selecione todos os anteoriores ao ultimo elemento.
@@ -541,7 +626,10 @@ class DropBoxController{
 
 
 
-      readFiles(){  
+      readFiles(){ 
+        
+        this.lastFolder = this.currentFolder.join('/');
+        
         this.getFirebaseRef().on('value', snapshot =>{
              //Limpar o conteúdo para popula-lo corretamente
               this.listFilesEl.innerHTML = '';
@@ -552,8 +640,11 @@ class DropBoxController{
                   let key = snapshotItem.key;
                   //Dados do objeto
                   let data = snapshotItem.val();
-                  //Adiciona o li que será criado em getFileView no ul(listFilesEl).
-                  this.listFilesEl.appendChild(this.getFileView(data, key))
+                  if(data.type){
+                     //Adiciona o li que será criado em getFileView no ul(listFilesEl).
+                     this.listFilesEl.appendChild(this.getFileView(data, key))
+                  }
+                   
               });
           }else{
             this.listFilesEl.innerHTML = '<li class="archiveMessageStyle" >Você não possui arquivos.</li>';
